2e0dc07c85a2eb53e8ea16d7cbdc29fb8a47674e
zoro <jagbetuyi001@gmail.com>
2022-05-30 00:01:55 +0100

    ejs

--- nodejs_demo/server.js
+++ nodejs_demo/server.js
@@ -1,3 +1,5 @@
+// WEBSITE : https://zellwk.com/blog/crud-express-mongodb/
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const MongoClient = require("mongodb").MongoClient;
@@ -5,26 +7,63 @@ const app = express();
 
 // Make sure you place body-parser before your CRUD handlers!
 app.use(bodyParser.urlencoded({ extended: true }));
+// THIS TELLS EXPRESS TO USE EJS AS TEMPLATE ENGINE
+app.set("view engine", "ejs");
 
-app.listen(3000, () => {
-  console.log("Listening on 3000");
-});
+// app.listen(3000, () => {
+//   console.log("Listening on 3000");
+// });
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
+// app.get("/", (req, res) => {
+//   res.sendFile(__dirname + "/index.html");
+// });
 
-app.post("/quotes", (req, res) => {
-  console.log(req.body);
-});
+// app.post("/quotes", (req, res) => {
+//   console.log(req.body);
+// });
 
+// CONNECTING TO MONGODB DB
 const connectionString =
   "mongodb+srv://marimo:marimo@atlascluster.mxgmp.mongodb.net/?retryWrites=true&w=majority";
-MongoClient.connect(
-  connectionString,
-  { useUnifiedTopology: true },
-  (err, client) => {
-    if (err) return console.error(err);
-    console.log("connected to the database");
-  }
-);
+
+// CONNECT TO MONGODB
+MongoClient.connect(connectionString, { useUnifiedTopology: true })
+  .then((client) => {
+    console.log("Connected to Database");
+    // NAME DB goingMerry
+    const db = client.db("goingMerry");
+    // CREATES A COLLECTION
+    const quoteCollection = db.collection("quotes");
+
+    app.listen(3000, () => {
+      console.log("Listening on 3000");
+    });
+
+    app.get("/", (req, res) => {
+      // res.sendFile(__dirname + "/index.html");
+      // GETS ALL THE QOUTES FROM DB
+      db.collection("quotes")
+        .find()
+        .toArray()
+        .then((result) => {
+          // RENDERS HTML
+          res.render("index.ejs", { quotes: result });
+        })
+        .catch((err) => console.log(err));
+    });
+
+    app.post("/quotes", (req, res) => {
+      // SAVES QUOTE TO COLLECTION ON DB
+      quoteCollection
+        .insertOne(req.body)
+        .then((result) => {
+          // console.log(result);
+          res.redirect("/");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+      // console.log(req.body);
+    });
+  })
+  .catch((error) => console.error(error));
